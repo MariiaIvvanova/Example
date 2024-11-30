6e3da0aa0081cb102ca6ca4fb05e0ab6a4b9ec75
MariiaIvvanova <ivanova_mariia36@mail.ru>
2024-11-30 14:48:04 +0300

    add "minus"

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def plus(a, b):
-    return a+b
+    return a + b
+
+
+def minus(a, b):
+    return a - b
 
 
 if __name__ == "__main__":
     a = 12
     b = 13
     print(plus(a, b))
+    print(minus(a, b))
